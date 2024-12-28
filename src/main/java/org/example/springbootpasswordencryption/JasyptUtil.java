89975e51600ffa18ad05a90b688a1263f47bdd15
rbhunia <rajbhunia34@gmail.com>
2024-12-28 13:23:48 +0530

    Initial commit

---
+++ src/main/java/org/example/springbootpasswordencryption/JasyptUtil.java
@@ -0,0 +1,4 @@
+package org.example.springbootpasswordencryption;
+
+public class JasyptUtil {
+}
---
+++ src/main/java/org/example/springbootpasswordencryption/SpringBootPasswordEncryptionApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootpasswordencryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPasswordEncryptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPasswordEncryptionApplication.class, args);
+    }
+
+}
